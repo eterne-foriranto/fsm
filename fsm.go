cc88c97eb12b7c44de9754e97071cb825dd7aae3
Yevgeni Starostin <eterne.foriranto@gmail.com>
2025-08-26 21:21:41 +0300

    feat: make a finite state machine

---
+++ fsm.go
@@ -0,0 +1,17 @@
+package fsm
+
+type Machine struct {
+	State   *State
+	Roadmap map[string]map[string]string
+	States  map[string]*State
+}
+
+func (m *Machine) ProcessInp(inp interface{}) {
+	upcode := m.State.ProcessInp(inp)
+	m.State = m.States[m.Roadmap[m.State.id][upcode]]
+}
+
+type State struct {
+	ProcessInp func(inp interface{}) string
+	id         string
+}
